d4e19aee3ec713d5ef612f8e515b0e74254fe8b7
Pablo <pablol.sordomartinez@gmail.com>
2018-04-09 13:02:00 +0200

    Add button2 functionality

--- app/src/main/java/org/bts/android/activitymanager/MainActivity.java
+++ app/src/main/java/org/bts/android/activitymanager/MainActivity.java
@@ -2,14 +2,22 @@ package org.bts.android.activitymanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLaunch = findViewById(R.id.btn_launch_activity2_activity_main);
+        btnLaunch.setOnClickListener(this);
+
+        Button btnKillAct = findViewById(R.id.btn_kill_activity_activity_main);
+        btnKillAct.setOnClickListener(this);
 
         Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
     }
@@ -54,5 +62,16 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
 
         Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.btn_launch_activity2_activity_main) {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.btn_kill_activity_activity_main)  {
+            finish();
+        }
+
     }
 }
