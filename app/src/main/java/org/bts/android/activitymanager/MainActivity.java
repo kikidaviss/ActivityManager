5c154b18382d09fefbb17327152a4f69b7f35e70
Pablo <pablol.sordomartinez@gmail.com>
2018-04-10 12:36:05 +0200

    Connection activities

--- app/src/main/java/org/bts/android/activitymanager/MainActivity.java
+++ app/src/main/java/org/bts/android/activitymanager/MainActivity.java
@@ -1,7 +1,10 @@
 package org.bts.android.activitymanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,56 +15,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
 
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    private void initViews() {
         Button btnLaunch = findViewById(R.id.btn_launch_activity2_activity_main);
         btnLaunch.setOnClickListener(this);
 
         Button btnKillAct = findViewById(R.id.btn_kill_activity_activity_main);
         btnKillAct.setOnClickListener(this);
-
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
 
-        Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -69,9 +75,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if (view.getId() == R.id.btn_launch_activity2_activity_main) {
             Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show();
-        } else if (view.getId() == R.id.btn_kill_activity_activity_main)  {
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            //startActivity(intent);
+            startActivityForResult(intent, 11);
+        } else if (view.getId() == R.id.btn_kill_activity_activity_main) {
             finish();
         }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("MY TAG", "requestCode: " + requestCode + ", resultCode: " + resultCode);
+
+        if (requestCode == 11 && resultCode == Activity.RESULT_OK) {
+
+            String inf =
+                    data.getStringExtra("result");
+
+            Toast.makeText(this,
+                    inf,
+                    Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 }
--- app/src/main/java/org/bts/android/activitymanager/SecondActivity.java
+++ app/src/main/java/org/bts/android/activitymanager/SecondActivity.java
@@ -1,13 +1,34 @@
 package org.bts.android.activitymanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private EditText etInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        etInfo = findViewById(R.id.et_input_info_activity_second);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String infoStr = etInfo.getText().toString();
+        Log.d("MY TAG", infoStr);
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("result", infoStr);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
     }
 }
